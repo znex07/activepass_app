b2093fcc3168a86ad3665be49c1fc3190487585b
znex07 <znex07@gmail.com>
2021-05-11 14:45:33 +0800

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,8 +2,6 @@
 
 @section('content')
 <div class="container d-flex justify-content-center">
-    @if (Auth::user()->isVerified)
-
     <div class="row  ">
         <div class="col-md-8 mt-3">
 
@@ -75,107 +73,6 @@
               </div>
             </div>
           </div>
-        @else
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card mt-5">
-                    <div class="card-header">{{ __('Verify Your Phone Number') }}</div>
-                    <div class="card-body">
-                        @if (session('error'))
-                        <div class="alert alert-danger" role="alert">
-                            {{session('error')}}
-                        </div>
-                        @endif
-                        {{Auth::user()->name}}
-                        Please enter the OTP sent to your number: {{Auth::user()->phone_number}}
-                        <form action="verify" method="post">
-                            @csrf
-                            <div class="form-group row">
-                                <label for="verification_code"
-                                    class="col-md-4 col-form-label text-md-right">{{ __('Phone Number') }}</label>
-                                <div class="col-md-6">
-                                    <input type="hidden" name="phone_number" value="{{Auth::user()->phone_number}}">
-                                    <input id="verification_code" type="tel"
-                                        class="form-control @error('verification_code') is-invalid @enderror"
-                                        name="verification_code" value="{{ old('verification_code') }}" required>
-                                    @error('verification_code')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                    @enderror
-                                </div>
-
-                            </div>
-
-                            <div class="form-group row mb-0">
-                                <div class="col-md-6 offset-md-4">
-                                    <button type="submit" class="btn btn-primary">
-                                        {{ __('Verify Phone Number') }}
-                                    </button>
-                                    <a class="btn btn-success mt-1 btn-sm" href="/send_otp">Send One Time Pin</a>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="modal" tabindex="-1" role="dialog">
-            <div class="modal-dialog" role="document">
-              <div class="modal-content">
-                <div class="modal-header">
-                  <h5 class="modal-title">Modal title</h5>
-                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                  </button>
-                </div>
-                <div class="modal-body">
-                  <p>Modal body text goes here.</p>
-                </div>
-                <div class="modal-footer">
-                  <button type="button" class="btn btn-primary">Save changes</button>
-                  <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                </div>
-              </div>
-            </div>
-          </div>
-<div class="card d-flex justify-content-center" style="width: 2.55in; height:4.25in;" >
-                <div class="title text-center mt-3">
-                    <img src="{{ Voyager::image(Auth::user()->avatar) }}" class="img-thumbnail" style="height: 70px; width:70px">
-                    <img src="/img/logo.png" class="img-thumbnail" style="width:60px; height:60px">
-
-                </div>
-                <div class="card-body text-center">
-                    <div class="row p-3" >
-                        {{-- <h3><b>Certificate of Vaccination</b></h3> --}}
-                        @foreach ($vaccine_status as $vax)
-                        @if ($vax->is_vaccinated == 'Fully Vaccinated')
-                            <label>This is to certify that the holder of this certificate has been Fully
-                                Vaccinated against Coronavirus <b>COVID-19</b></label>
-                                <div class="text-center col-12 mt-4">
-                                    <h3 class="text-success text-center"><i class="fa fa-check text-success"></i>Verified</h3>
-                                    <h5 class="text-center">Name: {{ $vax->name }}</h5>
-                                    <h5 class="text-center">Vaccine: {{ $vax->vaccine_brand }}</h5>
-                                    <small class="text-center">Date Vaccinated: {{ Carbon\Carbon::parse($vax->date_2)->format('Y-m-d') }}</small>
-                                </div>
-                            {{-- <small>Scan the QR below to verify status.</small> --}}
-                        @else
-                            <label>ActivePass digital vaccine passport for <b>COVID-19</b></label>
-                            {{-- <small>Scan the QR below to verify status.</small> --}}
-                            <h3 class="text-danger">Not Yet Vaccinated</h3>
-                        @endif
-                                {{-- {{ $vax->is_vaccinated  }} --}}
-                        @endforeach
-                    </div>
-                    <hr>
-                </div>
-                {{-- <button class="btn btn-success btn-sm">Send to your Email</button> --}}
-                {{-- <button class="btn btn-info mt-1 btn-sm">Send OTP</button> --}}
-                </div>
-            </div>
-
-        @endif
-
 
 <!-- Modal -->
 <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
