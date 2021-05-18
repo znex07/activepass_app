b85713b085d132f8cd2013f1291cff0cd8555a98
znex07 <znex07@gmail.com>
2021-05-18 08:40:29 +0800

    fixchat

--- resources/js/components/ChatMessages.vue
+++ resources/js/components/ChatMessages.vue
@@ -1,25 +1,12 @@
 <template>
-    <!-- <ul class="chat">
-        <li class="left clearfix" v-for="message in messages" :key="message.id">
-            <div class="chat-body clearfix">
-                <div class="header">
-                    <strong class="primary-font">
-                        {{ message.user.fname }}
-                    </strong>
-                </div>
-                <p>
-                    {{ message.message }}
-                </p>
-            </div>
-        </li>
-    </ul> -->
+
                 <!-- Conversations are loaded here -->
                 <div class="box-body py-3" style="padding:40px">
                   <!-- Message. Default to the left -->
 
                   <div class="direct-chat-msg" v-for="message in messages" :key="message.id">
                     <div class="direct-chat-info clearfix">
-                      <span class="direct-chat-name pull-left"> {{ message.user.fname }}</span>
+                      <span class="direct-chat-name pull-left" style="text-transform:capitalize"> {{ message.user.fname + ' ' + message.user.lname }}</span>
                     </div>
                     <!-- /.direct-chat-info -->
                     <img class="direct-chat-img" v-bind:src="'/img/'+ message.user.avatar" alt="message user image">
