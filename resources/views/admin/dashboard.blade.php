f31b75aeb67d68f45d3c3620e9cb1ed5f060428b
znex07 <znex07@gmail.com>
2021-05-21 16:23:20 +0800

    fix-dashboard-user

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -75,7 +75,7 @@
           </div><!-- /.col -->
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              {{-- <li class="breadcrumb-item"><a href="#">Home</a></li> --}}
             </ol>
           </div><!-- /.col -->
         </div><!-- /.row -->
@@ -129,6 +129,284 @@
 
 
           </div>
+          <section class="col-lg-5 connectedSortable">
+            <!-- DIRECT CHAT -->
+              <div class="card direct-chat direct-chat-primary" id="app_message">
+                <div class="card-header">
+                  <h3 class="card-title">
+                    <i class="fa fa-envelope"></i>
+                  Doctors Chat</h3>
+
+                  <div class="card-tools">
+                    <span title="3 New Messages" class="badge badge-primary">3</span>
+                    <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                      <i class="fas fa-minus"></i>
+                    </button>
+                    <button type="button" class="btn btn-tool" title="Contacts" data-widget="chat-pane-toggle">
+                      <i class="fas fa-comments"></i>
+                    </button>
+                    <button type="button" class="btn btn-tool" data-card-widget="remove">
+                      <i class="fas fa-times"></i>
+                    </button>
+                  </div>
+                </div>
+                <!-- /.card-header -->
+                <div class="card-body">
+                  <!-- Conversations are loaded here -->
+                   <chat-messages :messages="messages"></chat-messages>
+                  <!--/.direct-chat-messages-->
+
+                  <!-- Contacts are loaded here -->
+                  <div class="direct-chat-contacts">
+                    <ul class="contacts-list">
+                      <li>
+                        <a href="#">
+                          <img class="contacts-list-img" src="dist/img/user1-128x128.jpg" alt="User Avatar">
+
+                          <div class="contacts-list-info">
+                            <span class="contacts-list-name">
+                              Count Dracula
+                              <small class="contacts-list-date float-right">2/28/2015</small>
+                            </span>
+                            <span class="contacts-list-msg">How have you been? I was...</span>
+                          </div>
+                          <!-- /.contacts-list-info -->
+                        </a>
+                      </li>
+                      <!-- End Contact Item -->
+                      <li>
+                        <a href="#">
+                          <img class="contacts-list-img" src="dist/img/user7-128x128.jpg" alt="User Avatar">
+
+                          <div class="contacts-list-info">
+                            <span class="contacts-list-name">
+                              Sarah Doe
+                              <small class="contacts-list-date float-right">2/23/2015</small>
+                            </span>
+                            <span class="contacts-list-msg">I will be waiting for...</span>
+                          </div>
+                          <!-- /.contacts-list-info -->
+                        </a>
+                      </li>
+                      <!-- End Contact Item -->
+                      <li>
+                        <a href="#">
+                          <img class="contacts-list-img" src="dist/img/user3-128x128.jpg" alt="User Avatar">
+
+                          <div class="contacts-list-info">
+                            <span class="contacts-list-name">
+                              Nadia Jolie
+                              <small class="contacts-list-date float-right">2/20/2015</small>
+                            </span>
+                            <span class="contacts-list-msg">I'll call you back at...</span>
+                          </div>
+                          <!-- /.contacts-list-info -->
+                        </a>
+                      </li>
+                      <!-- End Contact Item -->
+                      <li>
+                        <a href="#">
+                          <img class="contacts-list-img" src="dist/img/user5-128x128.jpg" alt="User Avatar">
+
+                          <div class="contacts-list-info">
+                            <span class="contacts-list-name">
+                              Nora
+                              <small class="contacts-list-date float-right">2/10/2015</small>
+                            </span>
+                            <span class="contacts-list-msg">Where is your new...</span>
+                          </div>
+                          <!-- /.contacts-list-info -->
+                        </a>
+                      </li>
+                      <!-- End Contact Item -->
+                      <li>
+                        <a href="#">
+                          <img class="contacts-list-img" src="dist/img/user6-128x128.jpg" alt="User Avatar">
+
+                          <div class="contacts-list-info">
+                            <span class="contacts-list-name">
+                              John K.
+                              <small class="contacts-list-date float-right">1/27/2015</small>
+                            </span>
+                            <span class="contacts-list-msg">Can I take a look at...</span>
+                          </div>
+                          <!-- /.contacts-list-info -->
+                        </a>
+                      </li>
+                      <!-- End Contact Item -->
+                      <li>
+                        <a href="#">
+                          <img class="contacts-list-img" src="dist/img/user8-128x128.jpg" alt="User Avatar">
+
+                          <div class="contacts-list-info">
+                            <span class="contacts-list-name">
+                              Kenneth M.
+                              <small class="contacts-list-date float-right">1/4/2015</small>
+                            </span>
+                            <span class="contacts-list-msg">Never mind I found...</span>
+                          </div>
+                          <!-- /.contacts-list-info -->
+                        </a>
+                      </li>
+                      <!-- End Contact Item -->
+                    </ul>
+                    <!-- /.contacts-list -->
+                  </div>
+                  <!-- /.direct-chat-pane -->
+                </div>
+                <!-- /.card-body -->
+                <div class="card-footer">
+                  <form action="#" method="post">
+                    <div class="input-group">
+                      <chat-form
+                              v-on:messagesent="addMessage"
+                              :user="{{ Auth::user() }}" >
+
+                      </chat-form>
+                    </div>
+                  </form>
+                </div>
+                <!-- /.card-footer-->
+              </div>
+              <!--/.direct-chat -->
+              <!-- Calendar -->
+              <div class="card bg-gradient-success collapsed-card">
+                <div class="card-header border-0">
+
+                  <h3 class="card-title">
+                    <i class="far fa-calendar-alt"></i>
+                    Calendar
+                  </h3>
+                  <!-- tools card -->
+                  <div class="card-tools">
+                    <!-- button with a dropdown -->
+                    <div class="btn-group">
+                      <button type="button" class="btn btn-success btn-sm dropdown-toggle" data-toggle="dropdown" data-offset="-52">
+                        <i class="fas fa-bars"></i>
+                      </button>
+                      <div class="dropdown-menu" role="menu">
+                        <a href="#" class="dropdown-item">Add new event</a>
+                        <a href="#" class="dropdown-item">Clear events</a>
+                        <div class="dropdown-divider"></div>
+                        <a href="#" class="dropdown-item">View calendar</a>
+                      </div>
+                    </div>
+                    <button type="button" class="btn btn-success btn-sm" data-card-widget="collapse">
+                      <i class="fas fa-minus"></i>
+                    </button>
+                    <button type="button" class="btn btn-success btn-sm" data-card-widget="remove">
+                      <i class="fas fa-times"></i>
+                    </button>
+                  </div>
+                  <!-- /. tools -->
+                </div>
+                <!-- /.card-header -->
+                <div class="card-body pt-0">
+                  <!--The calendar -->
+                  <div id="calendar" style="width: 100%"></div>
+                </div>
+                <!-- /.card-body -->
+              </div>
+              <!-- Map card -->
+              <div class="card bg-gradient-primary collapsed-card">
+                <div class="card-header border-0">
+                  <h3 class="card-title">
+                    <i class="fas fa-map-marker-alt mr-1"></i>
+                    Healthcare Providers Branch Map
+                  </h3>
+                  <!-- card tools -->
+                  <div class="card-tools">
+                    <button type="button" class="btn btn-primary btn-sm daterange" title="Date range">
+                      <i class="far fa-calendar-alt"></i>
+                    </button>
+                    <button type="button" class="btn btn-primary btn-sm" data-card-widget="collapse" title="Collapse">
+                      <i class="fas fa-minus"></i>
+                    </button>
+                  </div>
+                  <!-- /.card-tools -->
+                </div>
+                <div class="card-body">
+                  <div id="makati-map" style="height: 250px; width: 100%;"></div>
+                </div>
+                <!-- /.card-body-->
+                <div class="card-footer bg-transparent">
+                  <div class="row">
+                    <div class="col-4 text-center">
+                      <div id="sparkline-1"></div>
+                      <div class="text-white">Visitors</div>
+                    </div>
+                    <!-- ./col -->
+                    <div class="col-4 text-center">
+                      <div id="sparkline-2"></div>
+                      <div class="text-white">Online</div>
+                    </div>
+                    <!-- ./col -->
+                    <div class="col-4 text-center">
+                      <div id="sparkline-3"></div>
+                      <div class="text-white">Sales</div>
+                    </div>
+                    <!-- ./col -->
+                  </div>
+                  <!-- /.row -->
+                </div>
+              </div>
+              <!-- /.card -->
+
+              <!-- solid sales graph -->
+              <div class="card bg-gradient-info collapsed-card">
+                <div class="card-header border-0">
+                  <h3 class="card-title">
+                    <i class="fas fa-th mr-1"></i>
+                    Doctors Graph
+                  </h3>
+
+                  <div class="card-tools">
+                    <button type="button" class="btn bg-info btn-sm" data-card-widget="collapse">
+                      <i class="fas fa-minus"></i>
+                    </button>
+                    <button type="button" class="btn bg-info btn-sm" data-card-widget="remove">
+                      <i class="fas fa-times"></i>
+                    </button>
+                  </div>
+                </div>
+                <div class="card-body">
+                  <canvas class="chart" id="line-chart" style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%;"></canvas>
+                </div>
+                <!-- /.card-body -->
+                <div class="card-footer bg-transparent">
+                  <div class="row">
+                    <div class="col-4 text-center">
+                      <input type="text" class="knob" data-readonly="true" value="20" data-width="60" data-height="60"
+                             data-fgColor="#39CCCC">
+
+                      <div class="text-white">VaxPass</div>
+                    </div>
+                    <!-- ./col -->
+                    <div class="col-4 text-center">
+                      <input type="text" class="knob" data-readonly="true" value="50" data-width="60" data-height="60"
+                             data-fgColor="#39CCCC">
+
+                      <div class="text-white">Patients</div>
+                    </div>
+                    <!-- ./col -->
+                    <div class="col-4 text-center">
+                      <input type="text" class="knob" data-readonly="true" value="30" data-width="60" data-height="60"
+                             data-fgColor="#39CCCC">
+
+                      <div class="text-white">Billing</div>
+                    </div>
+                    <!-- ./col -->
+                  </div>
+                  <!-- /.row -->
+                </div>
+                <!-- /.card-footer -->
+              </div>
+              <!-- /.card -->
+
+
+
+              <!-- /.card -->
+            </section>
           <!-- /.col-md-6 -->
           <div class="col-lg-6">
             {{-- <div class="card">
