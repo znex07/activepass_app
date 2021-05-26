4414ee14343dc52b3488a431de6534e98586e800
znex07 <znex07@gmail.com>
2021-05-26 10:23:49 +0800

    fix-dashboard-user

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -106,7 +106,7 @@
                 <ul class="nav nav-pills">
                   <li class="nav-item"><a class="nav-link active" href="#activity" data-toggle="tab">Activity</a></li>
                   <li class="nav-item"><a class="nav-link" href="#timeline" data-toggle="tab">Timeline</a></li>
-                  <li class="nav-item"><a class="nav-link" href="#settings" data-toggle="tab">Settings</a></li>
+                  {{-- <li class="nav-item"><a class="nav-link" href="#settings" data-toggle="tab">Settings</a></li> --}}
                 </ul>
               </div><!-- /.card-header -->
               <div class="card-body">
@@ -259,7 +259,7 @@
                   </div>
                   <!-- /.tab-pane -->
 
-                  <div class="tab-pane" id="settings">
+                  <div class="tab-pane d-none" id="settings">
                     <form class="form-horizontal">
                       <div class="form-group row">
                         <label for="inputName" class="col-sm-2 col-form-label">Name</label>
@@ -274,18 +274,12 @@
                         </div>
                       </div>
                       <div class="form-group row">
-                        <label for="inputName2" class="col-sm-2 col-form-label">Name</label>
+                        <label for="inputName2" class="col-sm-2 col-form-label">Message</label>
                         <div class="col-sm-10">
                           <input type="text" class="form-control" id="inputName2" placeholder="Name">
                         </div>
                       </div>
-                      <div class="form-group row">
-                        <label for="inputSkills" class="col-sm-2 col-form-label">Skills</label>
-                        <div class="col-sm-10">
-                          <input type="text" class="form-control" id="inputSkills" placeholder="Skills">
-                        </div>
-                      </div>
-                      <div class="form-group row">
+                      <div class="form-group row d-none">
                         <div class="offset-sm-2 col-sm-10">
                           <div class="checkbox">
                             <label>
