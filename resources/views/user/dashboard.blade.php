5b082fbe74ae396d7b10a4359a9b00999f35539c
znex07 <znex07@gmail.com>
2021-05-26 17:06:35 +0800

    fix-dashboard-user

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -83,7 +83,7 @@
           <a href="#" class="dropdown-item">
             <!-- Message Start -->
             <div class="media">
-              <img src="dist/img/user1-128x128.jpg" alt="User Avatar" class="img-size-50 mr-3 img-circle">
+              <img src="/img/plus.png" alt="User Avatar" class="img-size-50 mr-3 img-circle">
               <div class="media-body">
                 <h3 class="dropdown-item-title">
                   ActivePass
